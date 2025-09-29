99a98e395984924ba1c01574ccad1ad93292017e
Zafir Sk Heerah <zafirskthelifehacker@hotmail.com>
2025-09-29 16:54:07 +0400

    Fixes

--- backend/src/main/java/dev/zfir/springangtasks/security/SecurityConfig.java
+++ backend/src/main/java/dev/zfir/springangtasks/security/SecurityConfig.java
@@ -51,8 +51,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:3000"));
-        configuration.setAllowedOriginPatterns(Arrays.asList("https://spring-ang-tasks.fr.infra.zfir.dev"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200", 
+            "http://localhost:3000",
+            "https://spring-ang-tasks.fr.infra.zfir.dev"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
