e1cae9017d4a3a48504a1e2d1dd9a14aa96d6d89
Zafir Sk Heerah <zafirskthelifehacker@hotmail.com>
2025-09-29 13:36:52 +0400

    refactor

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('SpringAngTasks');
   });
 
-  it('should render title', () => {
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SpringAngTasks');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
